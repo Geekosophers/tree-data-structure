3f9a8abb773209a858c721bc899cb8a7d40b89d8
Anshul Sachdev <anshulsachdev@Anshuls-MacBook-Pro.local>
2021-07-25 00:02:30 +0530

    LeastCommonAncestor: Added algortihm highlighting

--- Binary Search Tree/LeastCommonAncestor/js/sketch.js
+++ Binary Search Tree/LeastCommonAncestor/js/sketch.js
@@ -5,6 +5,7 @@ var BST = null;
 var node = null;
 var n1 = null;
 var n2 = null;
+var isPlayed = false;
 
 function setup(preOrderNodes, node1, node2) {
 
@@ -39,6 +40,63 @@ function setup(preOrderNodes, node1, node2) {
     coord = [canvasDiv.offsetWidth/2,canvasDiv.offsetWidth/20];
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// async function highlightFunctionCall() {
+//     document.getElementById("statement1").style.backgroundColor = "#e14e87";
+//     await sleep(2000);
+//     document.getElementById('statement1').style.removeProperty("background-color");
+// }
+
+async function lca(node,n1,n2){
+    try{
+        for(let i=0;i<9;i++){
+            var statement = 'statement' + (i+1).toString();
+            document.getElementById(statement).style.removeProperty("background-color");
+        }
+    }catch(e){}
+    await sleep(2000)
+    if (node.data > n1 && node.data > n2) { // If both n1 and n2 are smaller than root then LCA lies in left
+        node.circle.color = "#e14e87";
+        await sleep(1000);
+        document.getElementById("statement2").style.backgroundColor = "#e14e87";
+        await sleep(2000);
+        document.getElementById("statement2").style.backgroundColor = "#4caf50";
+        await sleep(2000);
+        document.getElementById("statement3").style.backgroundColor = "#4caf50";
+        await sleep(2000);
+        lca(node.left,n1,n2);
+    } else if (node.data < n1 && node.data < n2) { // If both n1 and n2 are greater than root then LCA lies in right
+        node.circle.color = "#e14e87";
+        await sleep(1000);
+        document.getElementById("statement2").style.backgroundColor = "#e14e87";
+        await sleep(2000);
+        document.getElementById("statement4").style.backgroundColor = "#e14e87";
+        await sleep(2000);
+        document.getElementById("statement4").style.backgroundColor = "#4caf50";
+        await sleep(2000);
+        document.getElementById("statement5").style.backgroundColor = "#4caf50";
+        await sleep(2000);
+        lca(node.right,n1,n2);
+    } else {
+        node.circle.color = "#e14e87";
+        await sleep(1000);
+        document.getElementById("statement2").style.backgroundColor = "#e14e87";
+        await sleep(2000);
+        document.getElementById("statement4").style.backgroundColor = "#e14e87";
+        await sleep(2000);
+        document.getElementById("statement6").style.backgroundColor = "#4caf50";
+        await sleep(2000);
+        node.circle.color = "#4caf50";
+        document.getElementById("statement7").style.backgroundColor = "#4caf50";
+        play = false;
+        document.getElementById("play-button").innerHTML = "Play";
+        return node;
+    }
+}
+
 function draw() {
 
     if(preOrderNodes.length!=0){
@@ -50,21 +108,13 @@ function draw() {
 
         BST.postorder(BST.root); // draws the tree
         console.log('loop')
-        if(play===true){
+        if(isPlayed===false && play===true){
             document.getElementById("play-button").innerHTML = "Pause";
             
             // LCA logic
-            if (node.data > n1 && node.data > n2) { // If both n1 and n2 are smaller than root then LCA lies in left
-                node.circle.color = "#e14e87";
-                node = node.left;
-            } else if (node.data < n1 && node.data < n2) { // If both n1 and n2 are greater than root then LCA lies in right
-                node.circle.color = "#e14e87";
-                node = node.right;
-            } else {
-                node.circle.color = "#4caf50";
-                play = false;
-                document.getElementById("play-button").innerHTML = "Play";
-            }
+            // highlightFunctionCall()
+            lca(node,n1,n2)
+            isPlayed = true;
         }
         frameRate(1);
     }
