9247d70e390ead0b7bbd7c0d024404b981b2cb16
Anshul Sachdev <anshulsachdev@Anshuls-MacBook-Pro.local>
2021-07-25 03:43:17 +0530

    LeastCommonAncestor: Mobile view message added

--- Binary Search Tree/LeastCommonAncestor/js/control-scripts/pauseAnimation.js
+++ Binary Search Tree/LeastCommonAncestor/js/control-scripts/pauseAnimation.js
@@ -1,4 +1,5 @@
 var playAnimation = function(){
     play = !play;
+    play === false ? wasPaused=true : null;
     document.getElementById("play-button").innerHTML = play==true ? "Pause" : "Play";
 }
